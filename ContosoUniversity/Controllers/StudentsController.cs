d618f4466c3678cdb4dcd6769b68d547e031810a
Goesg <diegogoessg@gmail.com>
2025-06-01 14:32:37 -0300

    adicionando classificao por data e sobrenome dos estudantes, pagina index

--- ContosoUniversity/Controllers/StudentsController.cs
+++ ContosoUniversity/Controllers/StudentsController.cs
@@ -20,9 +20,22 @@ namespace ContosoUniversity.Controllers
         }
 
         // GET: Students
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(string sortOrder)
         {
-            return View(await _context.Students.ToListAsync());
+            ViewData["NameSortParm"] = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            ViewData["DateSortParm"] = sortOrder == "Date" ? "date_desc" : "Date";
+
+            var students = from s in _context.Students select s;
+
+            students = sortOrder switch
+            {
+                "name_desc" => students.OrderByDescending(s => s.LastName),
+                "Date" => students.OrderBy(s => s.EnrollmentDate),
+                "date_desc" => students.OrderByDescending(s => s.EnrollmentDate),
+                _ => students.OrderBy(s => s.LastName),
+            };
+            
+            return View(await students.AsNoTracking().ToListAsync());
         }
 
         // GET: Students/Details/5
