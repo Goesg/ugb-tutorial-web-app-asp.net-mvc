75a996e3db1e37dfc3ae38c9a718ca3f36cdaf37
Goesg <diegogoessg@gmail.com>
2025-05-31 23:38:53 -0300

    criando conexao com sql server via docker ou localdb

--- ContosoUniversity/Startup.cs
+++ ContosoUniversity/Startup.cs
@@ -15,9 +15,52 @@ public class Startup
 
     public void ConfigureServices(IServiceCollection services)
     {
-        services.AddDbContext<SchoolContext>(options =>
-            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+        ConectarNoBancoViaDocker(services);
+
+        //Filtro de exceção de banco de dados
+        services.AddDatabaseDeveloperPageExceptionFilter();
 
         services.AddControllersWithViews();
     }
+
+    //Utilizar essa conexao caso esteja rodando o projeto no Visual Studio
+    internal void ConectarNoBancoIntegradoAoVisualStudio(IServiceCollection services)
+    {
+        services.AddDbContext<SchoolContext>(options =>
+       options.UseSqlServer(Configuration.GetConnectionString("DockerDb")));
+    }
+
+    internal void ConectarNoBancoViaDocker(IServiceCollection services)
+    {
+        services.AddDbContext<SchoolContext>(options =>
+       options.UseSqlServer(Configuration.GetConnectionString("DockerDb")));
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+        else
+        {
+            app.UseExceptionHandler("/Home/Error");
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+        });
+    }
+
 }
